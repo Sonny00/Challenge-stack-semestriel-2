32f05f4aaf02949ef0f4be7e5e4c6a434befd0e8
Moufid17 <moufid.mtr@gmail.com>
2023-07-06 18:05:55 +0200

    Shipping page done !

--- front/src/.next/static/chunks/webpack.js
+++ front/src/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c2417e19778985c5"; }
+/******/ 		__webpack_require__.h = function() { return "c4a170907b03e2a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
